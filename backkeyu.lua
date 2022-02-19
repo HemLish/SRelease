0165a6f9c649ee49e4a8bb566f77a554446db0c0
HemLish <97192873+HemLish@users.noreply.github.com>
2022-02-19 23:55:01 +0100

    Update backkeyu.lua

--- backkeyu.lua
+++ backkeyu.lua
@@ -1,1 +1,13 @@
-
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "INFORMATION";
+Text = "Fixed Lag [GET THE SCRIPT AGAIN]"; 
+Duration = 5;
+})
+setclipboard("https://www.youtube.com/watch?v=Z3Jhomxwqoc&t=6s&ab_channel=HemLish")
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "LINK";
+Text = "LINK COPIED"; 
+Duration = 5;
+})
